ca78a65f8417a3560e12cf42324276eea13d7b04
gasiino <gasiino@gmail.com>
2018-02-10 20:26:59 +0000

    new script for file plot4

---
+++ plot4.R
@@ -0,0 +1,57 @@
+### plot4.R 
+### code to create plot4.png
+### NOTE: this scripts expects the file household_power_consumption.txt to be available on the same wrking directory as it runs
+### if the file is not existing in the current directory an attempt to download and unzip is done
+
+## using  library data.table 
+library(data.table)
+
+# if txt file and zip file are not here, attempt to download the zip file
+if(!file.exists("household_power_consumption.zip") && !file.exists("household_power_consumption.txt")) { 
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile = "household_power_consumption.zip",mode = "wb") 
+}
+# if txt file is not here but zip file is available, attempt to unzip the zip file here
+if(file.exists("household_power_consumption.zip") && !file.exists("household_power_consumption.txt")) { 
+  unzip("household_power_consumption.zip") 
+}
+## read whole txt file, then select only the rows using %like% and regex on Date column to get only 1 feb and 2 feb 2007
+dt<-fread("household_power_consumption.txt", header=TRUE, na.strings="?", sep = ";")[Date %like% "^[0]{0,1}[12]/[0]{0,1}2/2007"]
+
+## add column with datetime 
+dt$DateTime<-as.POSIXct(strptime(paste(dt$Date,dt$Time,Sep=" "),format="%d/%m/%Y %H:%M:%S"))
+## remove Date and Time columns
+dt[,Date := NULL]
+dt[,Time := NULL]
+
+## create png file with desired size
+png(filename="plot4.png", width=480, height=480, units="px")
+
+## create 4 plots
+par(mfrow=c(2,2), mar=c(3,4,1,1),mgp=c(2,1,0))
+
+## define colors
+metering1color<-"black"
+metering2color<-"red"
+metering3color<-"blue"
+
+## add first plot 
+with(dt, plot(DateTime, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)"))
+
+## add  second plot 
+with(dt, plot(DateTime, Voltage, type="l", xlab="datetime", ylab="Voltage"))
+
+## add third plot 
+with(dt, plot(DateTime, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering", col=metering1color))
+
+## add lines
+with(dt, lines(DateTime, Sub_metering_2,type="l", col=metering2color))
+with(dt, lines(DateTime, Sub_metering_3,type="l", col=metering3color))
+
+## add legend
+legend(x= "topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex=0.8, col=c(metering1color,metering2color,metering3color), lty=1, bty="n")
+
+## add fourth plot 
+with(dt, plot(DateTime, Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power"))
+
+## close device and save file
+dev.off()
